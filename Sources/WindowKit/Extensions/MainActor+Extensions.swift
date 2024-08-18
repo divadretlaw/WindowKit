3a9ce90d65862139ebd9a89d4082cf851fd70034
David Walter <hello@davidwalter.at>
2024-08-18 17:31:59 +0200

    Remove MainActor.runUnsafely

--- Sources/WindowKit/Extensions/MainActor+Extensions.swift
+++ Sources/WindowKit/Extensions/MainActor+Extensions.swift
@@ -10,29 +10,14 @@ import Foundation
 extension MainActor {
     /// Execute the given body closure on the main actor without enforcing MainActor isolation.
     ///
-    /// It will crash if run on any non-main thread.
-    @_unavailableFromAsync
-    static func runUnsafely<T>(_ body: @MainActor () throws -> T) rethrows -> T where T: Sendable {
-        if #available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *) {
-            return try MainActor.assumeIsolated(body)
-        } else {
-            dispatchPrecondition(condition: .onQueue(.main))
-            return try withoutActuallyEscaping(body) { function in
-                try unsafeBitCast(function, to: (() throws -> T).self)()
-            }
-        }
-    }
-    
-    /// Execute the given body closure on the main actor without enforcing MainActor isolation.
-    ///
     /// The method will be dispatched in sync to the main-thread if its on a non-main thread.
     @_unavailableFromAsync
     static func runSync<T>(_ body: @MainActor () throws -> T) rethrows -> T where T: Sendable {
         if Thread.isMainThread {
-            try MainActor.runUnsafely(body)
+            try MainActor.assumeIsolated(body)
         } else {
             try DispatchQueue.main.sync {
-                try MainActor.runUnsafely(body)
+                try MainActor.assumeIsolated(body)
             }
         }
     }
