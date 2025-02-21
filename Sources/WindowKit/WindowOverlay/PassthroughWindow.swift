2e689d5ebf019ed3e7abffa4e6ddb06287f231e9
David Walter <hello@davidwalter.at>
2025-02-21 21:34:07 +0100

    Add PassthroughWindow workaround

--- Sources/WindowKit/WindowOverlay/PassthroughWindow.swift
+++ Sources/WindowKit/WindowOverlay/PassthroughWindow.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 final class PassthroughWindow: UIWindow {
+    var allowNextRootHit = false
+    
+    // On root view hit this method will be called twice
+    //
+    // First we hit the view we want to actually hit and
+    // then right after the root view.
+    // Usually we want to pass the root view hit through,
+    // unless we hit the view we actually want to hit before.
+    //
+    // If we would prevent all root view hits, it would also
+    // prevent the hit of the view we want to hit.
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         guard let hitView = super.hitTest(point, with: event) else {
             return nil
         }
-        return rootViewController?.view == hitView ? nil : hitView
+        
+        guard let rootView = rootViewController?.view else {
+            return hitView
+        }
+        
+        defer {
+            allowNextRootHit = rootView.subviews.contains(hitView)
+        }
+        
+        if rootView == hitView {
+            return allowNextRootHit ? rootView : nil
+        } else {
+            return hitView
+        }
     }
 }
